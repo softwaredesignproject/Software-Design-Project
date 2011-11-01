9acc640c2cd61780a6f115ec6e2eee4278a2df08
Software Design Project - Android <harishkr@email.arizona.edu>
2011-11-01 00:47:51 -0700

    java sample commit

---
+++ fizzbuzz.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * @author Harish
+ *
+ */
+public class fizzbuzz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=100;i++)
+		{
+			if(i%3 == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else if(i%5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
