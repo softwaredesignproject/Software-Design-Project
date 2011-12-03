4dcfa7df4a9145751d20afe4f7236d26591092eb
Arthi J <arthij@email.arizona.edu>
2011-12-03 13:30:21 -0700

    UserData JSP Form Bean

---
+++ src/user/UserData.java
@@ -0,0 +1,98 @@
+package user;
+
+public class UserData {
+String startaddress;
+String endaddress;
+String rbtransport;
+String startzipcode;
+String endzipcode;
+String timetype;
+String date;
+String time;
+String rbweather;
+String rbsex;
+String rbage;
+String rbgroupsize;
+String rbtemp;
+
+
+public String getStartaddress() {
+	return startaddress;
+}
+public void setStartaddress(String startaddress) {
+	this.startaddress = startaddress;
+}
+public String getEndaddress() {
+	return endaddress;
+}
+public void setEndaddress(String endaddress) {
+	this.endaddress = endaddress;
+}
+public String getRbtransport() {
+	return rbtransport;
+}
+public void setRbtransport(String rbtransport) {
+	this.rbtransport = rbtransport;
+}
+public String getStartzipcode() {
+	return startzipcode;
+}
+public void setStartzipcode(String startzipcode) {
+	this.startzipcode = startzipcode;
+}
+public String getEndzipcode() {
+	return endzipcode;
+}
+public void setEndzipcode(String endzipcode) {
+	this.endzipcode = endzipcode;
+}
+public String getTimetype() {
+	return timetype;
+}
+public void setTimetype(String timetype) {
+	this.timetype = timetype;
+}
+public String getDate() {
+	return date;
+}
+public void setDate(String date) {
+	this.date = date;
+}
+public String getTime() {
+	return time;
+}
+public void setTime(String time) {
+	this.time = time;
+}
+public String getRbweather() {
+	return rbweather;
+}
+public void setRbweather(String rbweather) {
+	this.rbweather = rbweather;
+}
+public String getRbsex() {
+	return rbsex;
+}
+public void setRbsex(String rbsex) {
+	this.rbsex = rbsex;
+}
+public String getRbage() {
+	return rbage;
+}
+public void setRbage(String rbage) {
+	this.rbage = rbage;
+}
+public String getRbgroupsize() {
+	return rbgroupsize;
+}
+public void setRbgroupsize(String rbgroupsize) {
+	this.rbgroupsize = rbgroupsize;
+}
+public String getRbtemp() {
+	return rbtemp;
+}
+public void setRbtemp(String rbtemp) {
+	this.rbtemp = rbtemp;
+}
+
+}
